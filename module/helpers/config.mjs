9b8c2da53d213ce4f2795cc60122a5e4935b2a2b
DylanPitter <dylanpitter@gmail.com>
2023-05-22 11:42:18 -0300

    Initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const STELLARMISADVENTURES = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ STELLARMISADVENTURES.abilities = {
+  "str": "STELLARMISADVENTURES.AbilityStr",
+  "dex": "STELLARMISADVENTURES.AbilityDex",
+  "con": "STELLARMISADVENTURES.AbilityCon",
+  "int": "STELLARMISADVENTURES.AbilityInt",
+  "wis": "STELLARMISADVENTURES.AbilityWis",
+  "cha": "STELLARMISADVENTURES.AbilityCha"
+};
+
+STELLARMISADVENTURES.abilityAbbreviations = {
+  "str": "STELLARMISADVENTURES.AbilityStrAbbr",
+  "dex": "STELLARMISADVENTURES.AbilityDexAbbr",
+  "con": "STELLARMISADVENTURES.AbilityConAbbr",
+  "int": "STELLARMISADVENTURES.AbilityIntAbbr",
+  "wis": "STELLARMISADVENTURES.AbilityWisAbbr",
+  "cha": "STELLARMISADVENTURES.AbilityChaAbbr"
+};
